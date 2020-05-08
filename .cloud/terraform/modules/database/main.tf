2891c10d13f1b2c9198599ac8e47aafc757f31c2
VILLERS Mickaël <mickael.villers@epitech.eu>
2020-05-09 00:32:09 +0200

    update max connections sql

--- .cloud/terraform/modules/database/main.tf
+++ .cloud/terraform/modules/database/main.tf
@@ -17,6 +17,10 @@ resource google_sql_database_instance sql {
   settings {
     tier      = "db-f1-micro"
     disk_type = "PD_HDD"
+    database_flags {
+      name  = "max_connections"
+      value = "100"
+    }
   }
 }
 
